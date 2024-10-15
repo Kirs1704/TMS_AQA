75139a0e9f197557c515c8c5d943846f44942a1a
Kirill Sv <kirsonline91@gmail.com>
2024-10-15 15:16:35 +0700

    Start task

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -6,10 +6,36 @@ namespace TypesAndOperators
     {
         static void Main(string[] args)
         {
+
+            /*
+             float a1 = 12.5F;
+            float a2 = 12.5f;
+
+            decimal c1 = 1005.5M;
+            decimal c2 = 1005.5m;
+
+            //========= Неявная типизация =======================
+            var d = 10;
+            var d1 = 10.2F;
+            var d2 = 'D';
+            var d3 = "Hello";
+            */
             
-            Console.WriteLine("It's a nice day!");
+            // ======== Решение задачи ============
+            //Подготовка данных
             
+            bool isWhiteBreadFresh = true;
+            int WhiteBreadPrice = 150;
+            int ButterPrice = 230;
+            int MilkPrice = 170;
+            float MilkFatPerc = 1.2F;
             
+            Console.Write("Введите свой возраст: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(age);
+
+
+
         }
     }
 }
