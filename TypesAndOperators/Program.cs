c89b840f6a3138149c780b3e75f3b1058d8d882e
Kirill Sv <kirsonline91@gmail.com>
2024-10-16 13:37:42 +0700

    Task is done

--- TypesAndOperators/Program.cs
+++ TypesAndOperators/Program.cs
@@ -6,7 +6,6 @@ namespace TypesAndOperators
     {
         static void Main(string[] args)
         {
-
             /*
              float a1 = 12.5F;
             float a2 = 12.5f;
@@ -20,22 +19,62 @@ namespace TypesAndOperators
             var d2 = 'D';
             var d3 = "Hello";
             */
-            
+
             // ======== Решение задачи ============
             //Подготовка данных
-            
+
             bool isWhiteBreadFresh = true;
             int WhiteBreadPrice = 150;
             int ButterPrice = 230;
             int MilkPrice = 170;
             float MilkFatPerc = 1.2F;
-            
-            Console.Write("Введите свой возраст: ");
-            int age = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(age);
+            int iceCreamPrice = 200;
 
+            Console.Write("Введите начальную сумму: ");
+            int sum = Convert.ToInt32(Console.ReadLine());
+            if (sum > 0)
+            {
+                Console.WriteLine($"Отлично! Ваша начальная сумма: {sum} рублей");
+                if (isWhiteBreadFresh && sum>=WhiteBreadPrice)
+                {
+                    sum -= WhiteBreadPrice;
+                    Console.WriteLine($"Хлеб купили, у нас осталось {sum} рублей.");
+                }
 
+                if (sum >= ButterPrice)
+                {
+                    sum -= ButterPrice;
+                    Console.WriteLine($"Масло купили, у нас осталось {sum} рублей.");
+                }
+                else
+                {
+                    Console.WriteLine($"У нас осталось {sum} рублей. На масло этого не хватит.");
+                }
 
+                if (MilkFatPerc == 1.2F && sum >= MilkPrice)
+                {
+                    sum -= MilkPrice;
+                    Console.WriteLine($"Молоко купили, у нас осталось {sum} рублей.");
+                }
+                else
+                {
+                    Console.WriteLine("С молоком не сростается..Увы");
+                }
+
+                if (sum >= iceCreamPrice)
+                {
+                    sum -= iceCreamPrice;
+                    Console.WriteLine($"Шикарно! Даже мороженку купили.У нас осталось {sum} рублей.");
+                }
+                else
+                {
+                    Console.WriteLine("По мороженке отбой...");
+                }
+            }
+            else
+            {
+                Console.WriteLine("На это не разгуляешься..Сумма должна быть больше 0");
+            }
         }
     }
 }
