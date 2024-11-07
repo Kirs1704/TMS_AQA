3888ae353d99b8a82784a9b75dc5976d3dbf4b70
Kirill Sv <kirsonline91@gmail.com>
2024-11-07 14:58:25 +0700

    Init

---
+++ Methods/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Strings
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            
+        }
+    }
+}
