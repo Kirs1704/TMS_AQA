ead7a7d682d451426c8d0ab60e8f2149101f6263
Kirill Sv <kirsonline91@gmail.com>
2024-12-05 15:07:06 +0700

    2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace Task1
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+        }
+    }
+}
