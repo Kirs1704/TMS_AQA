b14d4d9f79b383f7b8c26f803496cdcc05a2c96b
Kirill Sv <kirsonline91@gmail.com>
2024-12-06 19:57:53 +0700

    StopTask3

---
+++ Task3/Bus.cs
@@ -0,0 +1,10 @@
+namespace Task3;
+
+public class Bus
+{
+    public string EndPoint { get; set; }
+    public int Number { get; set; }
+    public double Time { get; set; }
+    public int NumberOfSeats { get; set; }
+    
+}
